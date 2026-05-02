80853329ebf0acc0b31612fa449c8fb832f4e819
zium <133301179+zium1337@users.noreply.github.com>
2026-05-02 16:53:46 +0200

    fix auto-updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,8 @@ pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_fs::init())
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_process::init())
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
